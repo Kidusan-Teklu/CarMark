0eed182573993a786b1d5640d989dec5bdf02f41
Bethel TIlahun <163300739+betyy11@users.noreply.github.com>
2024-05-15 18:08:16 -0700

    Update script.js

---
+++ review1/script.js
@@ -0,0 +1,15 @@
+const allstar = document.queryselectorAll('.rating.star')
+allstar.foreach((item,idx)=>{
+  item.addEventlistner('click',function (){
+    allstar.foreach(=>{
+      i.classlist.replace('bxs-star','bx-star')
+    }
+                    }
+        for (let i=0; i<allstar.length; i++){
+    if(i<=idx)}
+  allstar[i].classlist.replace('bx-star','bxs=star')
+}
+                }
+                }}
+                }}
+    
