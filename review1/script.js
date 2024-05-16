4fea6a803313353959f232e6d41e47f4a3554051
Bethel TIlahun <163300739+betyy11@users.noreply.github.com>
2024-05-15 19:59:54 -0700

    Update script.js

--- review1/script.js
+++ review1/script.js
@@ -1,12 +1,14 @@
-const allstar = document.queryselectorAll('.rating.star')
+const allstar = document.queryselectorAll('.rating .star')
 const ratingvalue=document.queryselector('.rating input')
 allstar.foreach((item,idx)=>{
   item.addEventlistner('click',function (){
     let click=0
-    ratingvalue.value=
+    ratingvalue.value= idx + 1
+    console.log(ratingvalue.value)
     allstar.foreach(=>{
       i.classlist.replace('bxs-star','bx-star')
-      allstar[i].classlist.remove('active')
+      i.classlist.remove('active')
+      
     })
           
         for (let i=0; i<allstar.length; i++){
@@ -16,7 +18,9 @@ allstar.foreach((item,idx)=>{
       
 }else{
       allstar[i].style.setproperty('--i ,click)
-                  click++                 
+                  click++   
+
+      ]
                 }
                 }}
                 }}
