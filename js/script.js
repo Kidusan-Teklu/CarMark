b3f749e54faf9f7cebe672e310201ef674c0a704
Bethel Tilahun <163300739+betyy11@users.noreply.github.com>
2024-05-17 03:04:07 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,15 @@
-const allstar = document.queryselectorAll('.rating .star')
-const ratingvalue=document.queryselector('.rating input')
-allstar.foreach((item,idx)=>{
-  item.addEventlistner('click',function (){
-    let click=0
-    ratingvalue.value= idx + 1
-    console.log(ratingvalue.value)
-    allstar.foreach(=>{
-      i.classlist.replace('bxs-star','bx-star')
-      i.classlist.remove('active')
-      
-    })
-          
-        for (let i=0; i<allstar.length; i++){
-    if(i<=idx){
-  allstar[i].classlist.replace('bx-star','bxs=star')
-    allstar[i].classlist.add('active')
-      
-}else{
-      allstar[i].style.setproperty('--i ,click)
-                  click++  ; 
-
-
-    }
-                }
-                }}
+document.addEventListener('DOMContentLoaded', function() {
+  const stars = document.querySelectorAll('.star');
+  
+  stars.forEach(function(star, index) {
+      star.addEventListener('click', function() {
+          for (let i = 0; i < stars.length; i++) {
+              if (i <= index) {
+                  stars[i].classList.add('active');
+              } else {
+                  stars[i].classList.remove('active');
+              }
+          }
+      });
+  });
+});
